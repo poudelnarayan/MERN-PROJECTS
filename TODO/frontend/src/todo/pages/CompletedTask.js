613d2e6cec25a86c312ed15df8c66b84d988a1d5
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-06-09 23:00:28 +0545

    added components

--- TODO/frontend/src/todo/pages/CompletedTask.js
+++ TODO/frontend/src/todo/pages/CompletedTask.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./CompletedTask.css";
 
 import TodoList from "../components/TodoList";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/ErrorModal";
+import LoadingSpinner from "../../shared/components/LoadingSpinner";
 
 const CompletedTask = (props) => {
+  const [loadedTasks, setLoadedTasks] = useState([]);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/tasks/completed"
+        );
+        setLoadedTasks(responseData.activeTasks);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTasks();
+  }, [sendRequest, setLoadedTasks]);
+
   return (
     <React.Fragment>
-      <div className="wrapper">
-        <TodoList
-          items={props.items}
-          onEdit={props.onEdit}
-          onDelete={props.onDelete}
-          onTaskComplete={props.onTaskComplete}
-        />
-      </div>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && (
+        <div className="wrapper">
+          <TodoList
+            items={loadedTasks}
+            onEdit={props.onEdit}
+            onDelete={props.onDelete}
+            onTaskComplete={props.onTaskComplete}
+          />
+        </div>
+      )}
     </React.Fragment>
   );
 };
